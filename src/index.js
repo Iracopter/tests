b00db97ddb594e8ef5dc76644634e5393535ad5c
Iracopter <114257500+Iracopter@users.noreply.github.com>
2023-04-15 20:44:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,67 +1,20 @@
-function Sum(num1, num2){
-    if(typeof(num1)=='number'&&typeof(num2)=='number'){
-        return num1+num2;
+var n=0;
+function changesize() {
+    var image = document.getElementById('my_photo'); // отримуємо елемент зображення за його ідентифікатором
+    if(n==0){
+        image.style.transform = 'scale(1.75)';
+        n++;
     }
     else{
-        return ("You should use numbers!")
+        image.style.transform = 'scale(1)';
+        n--;
     }
 }
 
-function Minus(num1, num2){
-    if(typeof(num1)=='number'&&typeof(num2)=='number'){
-        return num1-num2;
-    }
-    else{
-        return ("You should use numbers!")
-    }
-}
 
-function Dobutok(num1, num2){
-    if(typeof(num1)=='number'&&typeof(num2)=='number'){
-        return num1*num2;
-    }
-    else{
-        return ("You should use numbers!")
-    }
-}
+function keypress(event){
+    var hiddenElement = document.getElementById(event.key);
+    hiddenElement.scrollIntoView({block: "center", behavior: "smooth"});
+};
 
-function Dillenia(num1, num2){
-    if(typeof(num1)=='number'&&typeof(num2)=='number'){
-        if(num2!=0)
-        return num1/num2;
-        else return ("Ділити на нуль не можна!");
-    }
-    else{
-        return ("You should use numbers!")
-    }
-}
-
-console.log(Sum("2",4));
-console.log(Minus(2,4));
-console.log(Dobutok(2,"nkfnfk"));
-console.log(Dillenia(2,0));
-
-function Max(numbers){
-    let max=numbers[0];
-    for(let i=1; i<numbers.length; i++){
-        if(numbers[i]>max){
-            max=numbers[i];
-        }
-    }
-    return max;
-}
-
-function Min(numbers){
-    let min=numbers[0];
-    for(let i=1; i<numbers.length; i++){
-        if(numbers[i]<min){
-            min=numbers[i];
-        }
-    }
-    return min;
-}
-
-let numbers=[1,4,7,8,9,4,3,-2,6,8,10,6,4];
-
-console.log(Max(numbers));
-console.log(Min(numbers));
+document.addEventListener('keydown', keypress);
